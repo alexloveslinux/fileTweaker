6de457c8a2c7440942179e84e150e069207b0b81
skyc777-sudo <alexgagoshidze2013+lol@gmail.com>
2024-12-30 16:05:47 +0400

    Implement file operations in tkinter GUI: add create, modify, delete, and read functionalities

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,38 +1,40 @@
-# Modules
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import simpledialog as sd
+from tkinter import messagebox as mb
+from tkinter import ttk
 from os import *
 from shutil import *
-import os
 
-# Class
-class App:
-    # Main function
-    def __init__(self, root):
-        self.root = root
-        self.root.title('PWK')
-        self.root.geometry('200x100')
-        self.copy = Button(self.root, text='Copy', command=self.copy)
-        self.copy.pack()
-        self.delete = Button(self.root, text='Delete', command=self.delete)
-        self.delete.pack()
-    # Copy function
-    def copy(self):
-        filenames = [('All files', '*.*')]
-        source = fd.askopenfilename(filetypes=filenames, defaultextension=filenames)
-        if source:
-            destination_dir = fd.askdirectory()
-            if destination_dir:
-                destination = os.path.join(destination_dir, os.path.basename(source))
-                copy2(source, destination)
-    # Delete function
-    def delete(self):
-        filenames = [('All files', '*.*')]
-        deletefile = fd.askopenfilename(filetypes=filenames, defaultextension=filenames)
-        if deletefile:
-            remove(deletefile)
-# Main loop
+fn = [('All files', '*.*')]
+
+def createfile():
+    fd.asksaveasfile(mode='x', filetypes=fn, defaultextension=fn)
+
+def modifyfile():
+    modifyfile = fd.askopenfile(mode='r+', filetypes=fn, defaultextension=fn)
+    edit = sd.askstring('Edit', 'Enter text to add:')
+    modifyfile.write(edit)
+
+def deletefile():
+    deletefile = fd.askopenfile(filetypes=fn, defaultextension=fn)
+    deletefile.close()
+    remove(deletefile.name)
+
+def readfile():
+    readfile = fd.askopenfile(mode='r', filetypes=fn, defaultextension=fn)
+    mb.showinfo('File content', readfile.read())
+
 root = Tk()
-app = App(root)
+root.title('PWK')
+root.geometry('200x200')
+root.resizable(False, False)
+create = ttk.Button(root, text='Create file', command=createfile)
+create.pack(pady=10)
+modify = ttk.Button(root, text='Modify file', command=modifyfile)
+modify.pack(pady=10)
+delete = ttk.Button(root, text='Delete file', command=deletefile)
+delete.pack(pady=10)
+readbtn = ttk.Button(root, text='Read file', command=readfile)
+readbtn.pack(pady=10)
 root.mainloop()
